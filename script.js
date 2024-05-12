89b809ae014948cb2458cb2ee73416e377809d21
zipcord24 <134021284+zipcord24@users.noreply.github.com>
2024-05-12 09:56:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("startButton").addEventListener("click", function () {
+    console.log("hi")
+});
+
+window.addEventListener("scroll", function() {
+    window.scrollTo(0, 0);
+});
